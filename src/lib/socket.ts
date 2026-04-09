bbf015ce1bd67e3822ab44edd784f93c89640f45
Tehmina11-dev <tehminadev@gmail.com>
2026-04-09 16:26:44 +0500

    Update socket backend URL

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 // lib/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://chat-app-production-09f2.up.railway.app", {
   autoConnect: false,
 });
